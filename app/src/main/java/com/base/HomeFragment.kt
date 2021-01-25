12cae3659cf1740ecdb2486e112e0a23e1f6f515
kystudio <yuan012308@hotmail.com>
2021-01-25 14:17:04 +0800

    微调

--- app/src/main/java/com/base/HomeFragment.kt
+++ app/src/main/java/com/base/HomeFragment.kt
@@ -8,6 +8,9 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
     override fun initBinding(view: View): FragmentHomeBinding = FragmentHomeBinding.bind(view)
 
     override fun init() {
+        binding.titleBar.tvTitle.text = "ViewBinding Demo"
+        binding.titleBar.ivReturn.visibility = View.VISIBLE
+        binding.titleBar.ivReturn.setOnClickListener {  }
         binding.tvInfo.text = "HomeFragment by viewBinding"
     }
 }
--- app/src/main/java/com/base/MainActivity.kt
+++ app/src/main/java/com/base/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.base
 
-import android.view.View
 import com.base.databinding.ActivityMainBinding
 
 class MainActivity : BaseActivity<ActivityMainBinding>() {
@@ -8,10 +7,7 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
     override fun getViewBinding(): ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
 
     override fun initData() {
-//        setStatusBar(1)
-        binding.tvInfo.text = "MainActivity by viewBinding"
-        binding.titleBar.tvTitle.text = "ViewBinding Demo"
-        binding.titleBar.ivReturn.visibility = View.VISIBLE
-        binding.titleBar.ivReturn.setOnClickListener { finish() }
+        setStatusBar(0)
+        binding.tvInfoActivity.text = "MainActivity by viewBinding"
     }
 }
